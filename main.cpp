31010ee4100968a4da7ae04e659f7c627c772c40
m0nteir0 <93258691+m0nteir0@users.noreply.github.com>
2023-03-09 14:48:11 +0000

    Tested Max Flow

--- main.cpp
+++ main.cpp
@@ -5,11 +5,11 @@
 
 int main() {
     Data d;
-    bool a = d.readStations("../Data/Project1Data/stations.csv");
-    bool b = d.readNetworks("../Data/Project1Data/network.csv");
+    bool a = d.readStations("../Data/Project1Data/stations1.csv");
+    bool b = d.readNetworks("../Data/Project1Data/network1.csv");
     cout << endl << a << endl;
     cout << b << endl;
-
+    cout << d.getMaxFlow(2,6);
 
     cout << "Done";
     Interface iFace = Interface();
